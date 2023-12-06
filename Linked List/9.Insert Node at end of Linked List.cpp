f75ffa844eeef22ceb570b2eceb58a6c69ae7f37
Raj Gupta <91501288+rajgupta007@users.noreply.github.com>
2023-12-06 23:07:14 +0530

    9.Insert Node at end of Linked List.cpp

---
+++ Linked List/9.Insert Node at end of Linked List.cpp
@@ -0,0 +1,30 @@
+/************************************************************
+
+    Following is the linkedList class structure:
+
+    class Node {
+    public:
+        int data;
+        Node *next;
+
+        Node(int val) {
+            this->data = val;
+            next = NULL;
+        }
+    };
+
+************************************************************/
+
+Node* insertEnd(Node* head, int k) {
+    // Write your code here.
+    if(head == NULL)return (new Node(k));
+
+    Node* temp = head;
+    while(temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    Node* lastNode = new Node(k);
+    temp->next = lastNode;
+    return head;
+}
